21e98e77f53a5752a66a26a9f07fb0cae49751fd
Erik Hromek <erikhromek@julasoft.com>
2026-09-11 00:24:16 -0300

    Se agregan ejercicios de la práctica 3

---
+++ practica_3/Drivers/API/Src/API_delay.c
@@ -0,0 +1,34 @@
+#include "API_delay.h"
+
+
+void delayInit(delay_t *delay, tick_t duration) {
+	delay->startTime = 0;
+	delay->duration = duration;
+	delay->running = false;
+
+}
+
+bool_t delayRead(delay_t *delay) {
+	bool_t completed = false;
+	tick_t currentTime = HAL_GetTick();
+
+	if (delay->running) {
+		completed = (currentTime - delay->startTime >= delay->duration);
+		if (completed)
+			delay->running = false;
+	}
+	else {
+		delay->startTime = currentTime;
+		delay->running = true;
+	}
+	return completed;
+}
+
+void delayWrite( delay_t * delay, tick_t duration ) {
+	if (delay != NULL)
+		delay->duration = duration;
+}
+
+bool_t delayIsRunning(delay_t * delay) {
+	return delay->running;
+}
